2dcdc165fa5fe2dead3fef4af5c35b5e5e6f22e0
Paiste1 <alexpaiste.ap@gmail.com>
2019-12-23 12:56:39 +0800

    v. 0.5 - создал класс для кеширования

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use ishop\Cache;
 use \RedBeanPHP\R as R; // тут
 
 class MainController extends AppController
@@ -12,7 +13,15 @@ class MainController extends AppController
         $this->setMeta( 'Главная страница', 'описание...', 'Ключевые фразы...');
         $name = 'John';
         $age = 30;
-        $names = ['Alex', 'Johny', 'Anton'];
+        $names = ['Alex', 'Johny', 'Anton', 'Ololo'];
+        $cache = Cache::instanse();
+//        $cache->set('test', $names);
+//        $cache->delete('test');
+        $data = $cache->get('test');
+        if(!$data){
+            $cache->set('test', $names);
+        }
+        debug($data);
         $this->set(compact('name', 'age', 'names', 'posts'));
     }
 }
